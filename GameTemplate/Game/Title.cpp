baa204098a51726e6fce8f9767ec08bbf9c46d6a
FukumiToshiaki <osotosshi0@icloud.com>
2025-02-08 14:15:36 +0900

    ddsの変更

--- GameTemplate/Game/Title.cpp
+++ GameTemplate/Game/Title.cpp
@@ -9,7 +9,7 @@ Title::~Title()
 bool Title::Start()
 {
 	//タイトルの画像
-	m_spriteRender_title.Init("Assets/modelData/test/Test_Start.DDS", 1920.0f, 1080.0f);
+	m_spriteRender_title.Init("Assets/modelData/test/Test_Start.dds", 400.0f, 400.0f);
 
 	//Sprite表示用のシェーダーのファイルパスを指定する。
 	return true;
@@ -23,6 +23,8 @@ void Title::Render(RenderContext& rc)
 
 void Title::Update()
 {
+	//画像の更新。
+	m_spriteRender_title.Update();
 	//Aボタンを押したら
 	if (g_pad[0]->IsTrigger(enButtonA)) {
 		NewGO<Game>(0, "game");
