5faa6d8742efcad747d78eeec4babd1c62e2fd89
FukumiToshiaki <osotosshi0@icloud.com>
2025-02-15 14:36:16 +0900

    ステートの変更

--- GameTemplate/Game/BossState_Landing.cpp
+++ GameTemplate/Game/BossState_Landing.cpp
@@ -7,10 +7,12 @@ void BossState_Landing::Animation()
 	m_enemy_Boss->GetModelRender().PlayAnimation(
 		Enemy_Boss::enState_Landing_Animation, 0.2f
 	);
-	if (!m_enemy_Boss->GetIsPlayingAnimation())
-	{
+	if (m_enemy_Boss->GetisScream_Set() && !m_enemy_Boss->GetIsPlayingAnimation()) {
 		//™ôšK—p
 		m_enemy_Boss->SetisScream_Normal(true);
+	}
+	else if (!m_enemy_Boss->GetIsPlayingAnimation())
+	{
 		//Idleó‘Ô‚É‚·‚é
 		m_enemy_Boss->ChangeState(Enemy_Boss::enState_Idle);
 	}
@@ -18,5 +20,4 @@ void BossState_Landing::Animation()
 
 void BossState_Landing::Update()
 {
-
 }
