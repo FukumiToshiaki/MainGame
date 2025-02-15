ae5a85335d82d95a4d5ff16d3c27c6c5c4419940
FukumiToshiaki <osotosshi0@icloud.com>
2025-02-15 14:42:53 +0900

    制限時間の変更　実装はまだ

--- GameTemplate/Game/Time_Limit_UI.cpp
+++ GameTemplate/Game/Time_Limit_UI.cpp
@@ -1,27 +1,45 @@
 #include "stdafx.h"
-#include "Time_Limit_UI.h"
-
-void Time_Limit_UI::Update()
-{
-	wchar_t wcsbuf[256];
-	swprintf_s(wcsbuf, 256, L"%d", int(m_time_Limit));
-
-	//表示するテキストを設定。
-	m_fontRender.SetText(wcsbuf);
-	//フォントの位置を設定。
-	m_fontRender.SetPosition(Vector3(0.0f, 0.0f, 0.0f));
-	//フォントの大きさを設定。
-	m_fontRender.SetScale(3.5f);
-	//フォントの色を設定。
-	m_fontRender.SetColor({ 1.0f,1.0f,1.0f,1.0f });
-
-	m_time_Limit -= g_gameTime->GetFrameDeltaTime();
-
-
-}
-
-void Time_Limit_UI::Render(RenderContext& rc)
-{
-	//フォントを描画する。
-	m_fontRender.Draw(rc);
-}
+//#include "Time_Limit_UI.h"
+//
+//namespace {
+//	//HPの位置
+//	const Vector3 n_Time_Position{ -760.0f,-360.0f,0.0f };
+//	//HPの位置
+//	const Vector3 n_Time_Position_Red{ -760.0f,-360.0f,0.0f };
+//	//BとCの基点
+//	const Vector2 n_pivot_HP_Damage{ 0,0.5 };
+//	//delaytimerの初期設定時間
+//	float m_delaytime = 0.7f;
+//}
+//
+//void Time_Limit_UI::Update()
+//{
+//	//wchar_t wcsbuf[256];
+//	//swprintf_s(wcsbuf, 256, L"%d", int(m_time_Limit));
+//
+//	////表示するテキストを設定。
+//	//m_fontRender.SetText(wcsbuf);
+//	////フォントの位置を設定。
+//	//m_fontRender.SetPosition(Vector3(0.0f, 0.0f, 0.0f));
+//	////フォントの大きさを設定。
+//	//m_fontRender.SetScale(3.5f);
+//	////フォントの色を設定。
+//	//m_fontRender.SetColor({ 1.0f,1.0f,1.0f,1.0f });
+//
+//	m_time_Limit -= g_gameTime->GetFrameDeltaTime();
+//
+//
+//}
+//
+//void Time_Limit_UI::Test()
+//{
+//	//角度をラジアン単位に変換します
+//	float angle = (m_degree * PI) / 180.0f;
+//
+//}
+//
+//void Time_Limit_UI::Render(RenderContext& rc)
+//{
+//	//フォントを描画する。
+//	m_fontRender.Draw(rc);
+//}
--- GameTemplate/Game/Time_Limit_UI.h
+++ GameTemplate/Game/Time_Limit_UI.h
@@ -1,25 +1,93 @@
-#pragma once
-#include <FontRender.h>
-class Time_Limit_UI : public IGameObject
-{
-public:
-	Time_Limit_UI() {};
-	~Time_Limit_UI() {};
-	//bool Start();
-	void Update();
-	void Render(RenderContext& renderContext);//モデル
-
-	float GetTime_Limit()
-	{
-		//制限時間
-		return m_time_Limit;
-	}
-private:
-	//////////////////////////////////////
-	// メンバ変数。
-	//////////////////////////////////////
-	FontRender m_fontRender;				//レンダー
-
-	float m_time_Limit = 900.0f;				//制限時間　60秒
-};
-
+//#pragma once
+//#include <FontRender.h>
+//#include <SpriteRender.h>
+//const float PI = 3.14159f;
+//
+//#define DecreaseTransitionIN0	m_decrease_TRAN = en_Standby_TRAN;
+//#define DecreaseTransitionIN1	m_decrease_TRAN = en_DelayTimeON_TRAN
+//#define DecreaseTransitionIN2	m_decrease_TRAN = en_DelayTimeLoop_TRAN;
+//#define DecreaseTransitionIN3	m_decrease_TRAN = en_TransparentDecreaseCalc_TRAN;
+//#define DecreaseTransitionIN4	m_decrease_TRAN = en_TransparentDecreaseON_TRAN
+//#define DecreaseTransition0 m_decrease_TRAN == en_Standby_TRAN
+//#define DecreaseTransition1 m_decrease_TRAN == en_DelayTimeON_TRAN
+//#define DecreaseTransition2	m_decrease_TRAN == en_DelayTimeLoop_TRAN
+//#define DecreaseTransition3	m_decrease_TRAN == en_TransparentDecreaseCalc_TRAN
+//#define DecreaseTransition4	m_decrease_TRAN == en_TransparentDecreaseON_TRAN
+//
+//class Math;
+//class Time_Limit_UI : public IGameObject
+//{
+//public:
+//	//透過UI減少還移
+//	enum DecreaseTransition {
+//		//待機0
+//		en_Standby_TRAN,
+//		//遅延開始1
+//		en_DelayTimeON_TRAN,
+//		//遅延処理2
+//		en_DelayTimeLoop_TRAN,
+//		//透過UI減少計算3
+//		en_TransparentDecreaseCalc_TRAN,
+//		//透過UI減少開始4
+//		en_TransparentDecreaseON_TRAN
+//	};
+//
+//	Time_Limit_UI() {};
+//	~Time_Limit_UI() {};
+//	//bool Start();
+//	void Update();
+//	void Test();
+//	void Render(RenderContext& renderContext);//モデル
+//
+//	float GetTime_Limit()
+//	{
+//		//制限時間
+//		return m_time_Limit;
+//	}
+//private:
+//	//////////////////////////////////////
+//	// メンバ変数。
+//	//////////////////////////////////////
+//	FontRender m_fontRender;				//レンダー
+//	/////////////////////////////////////////メンバ変数
+////HPUI
+//	SpriteRender m_hpUI_A;
+//	//HPバー
+//	SpriteRender m_hpUI_B;
+//	//透過HPバー
+//	SpriteRender m_hpUI_C;
+//	//透過UI減少遷移
+//	DecreaseTransition m_decrease_TRAN = en_Standby_TRAN;
+//	/////////////////////////////////////////変数
+//	//最大HP
+//	float m_fullhp = 50;
+//	//現在HP　
+//	float m_nowhp = m_fullhp;
+//	//過去HP
+//	float m_oldhp = m_fullhp;
+//	//現在hpバースケール(動かすのはm_hpUI_B、zは固定)
+//	Vector3 m_scale_B = { 1.0,1.0,1.0 };
+//	//現在透過hpバースケール(動かすのはm_hpUI_C、zは固定)
+//	Vector3 m_scale_C = { 1.0,1.0,1.0 };
+//	//減少遅延タイマー
+//	float m_delaytimer = 0.0f;
+//	//透過減少割合
+//	Vector3 m_changeS_C = m_scale_C;
+//	//現在透過減少値
+//	Vector3 m_nowScale = m_hpUI_C.GetScale();
+//	//プレイヤーの表示のポジション
+//	Vector3 m_pos = { 0.0f,0.0f,0.0f };
+//	//プレイヤーの表示の最初のポジション
+//	Vector3 m_firstPos = { 0.0f,0.0f,0.0f };
+//	//スプライトポジション
+//	Vector2 m_spritePos = Vector2::Zero;
+//	//スプライトレンダー
+//	SpriteRender m_spriteRender;
+//	//角度
+//	float m_degree = 360.0f;   
+//	/////////////////////////////////////////初期設定系統
+//
+//
+//	float m_time_Limit = 900.0f;				//制限時間　60秒
+//};
+//
